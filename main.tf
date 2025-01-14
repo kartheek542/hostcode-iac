9647fce6e66f8b4678ee2abd77b972bde6cbdc20
kartheek542 <kartheekdivvela@gmail.com>
2025-01-14 13:30:42 +0530

    Removed slave instance

--- main.tf
+++ main.tf
@@ -21,27 +21,27 @@ resource "aws_key_pair" "key" {
     public_key = file("terr_aws_key.pub")
 }
 
-resource "aws_instance" "kube_slave" {
-    count = 2
-    ami = var.ami_id
-    instance_type = var.instance_type
-    key_name = aws_key_pair.key.key_name
-    vpc_security_group_ids = [aws_security_group.vpc_sg.id]
-    subnet_id = aws_subnet.instance_subnet.id 
-    private_ip = var.rem_private_ips[count.index]
-    associate_public_ip_address = true
-    user_data = file("ec2-setup.sh")
-
-    ebs_block_device {
-      device_name = "/dev/xvde"
-      delete_on_termination = true
-      volume_size = 10
-    }
-
-    tags = {
-        Name = var.rem_instance_names[count.index]
-    }
-} 
+# resource "aws_instance" "kube_slave" {
+#     count = 2
+#     ami = var.ami_id
+#     instance_type = var.instance_type
+#     key_name = aws_key_pair.key.key_name
+#     vpc_security_group_ids = [aws_security_group.vpc_sg.id]
+#     subnet_id = aws_subnet.instance_subnet.id 
+#     private_ip = var.rem_private_ips[count.index]
+#     associate_public_ip_address = true
+#     user_data = file("ec2-setup.sh")
+# 
+#     ebs_block_device {
+#       device_name = "/dev/xvde"
+#       delete_on_termination = true
+#       volume_size = 10
+#     }
+# 
+#     tags = {
+#         Name = var.rem_instance_names[count.index]
+#     }
+# } 
 
 resource "aws_instance" "kube_master" {
     ami = var.ami_id
--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "kube_master_instance_public_ip" {
     value = aws_instance.kube_master.public_ip
 }
 
-output "kube_slave_instance_public_ip" {
-    description = "Public IP's of EC2 instances"
-    value = aws_instance.kube_slave[*].public_ip
-}
+# output "kube_slave_instance_public_ip" {
+#     description = "Public IP's of EC2 instances"
+#     value = aws_instance.kube_slave[*].public_ip
+# }
