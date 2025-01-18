1de843ec830e40f1f3aa1886a7240be38833b6b4
kartheek542 <kartheekdivvela@gmail.com>
2025-01-18 11:18:59 +0530

    Created Dev branch for dev purpose

--- outputs.tf
+++ outputs.tf
@@ -3,17 +3,17 @@ output "kube_master_public_ip" {
     value = aws_instance.kube_master.public_ip
 }
 
-output "kube_slave_public_ip" {
-    description = "Public IP's of EC2 instances"
-    value = aws_instance.kube_slave[*].public_ip
-}
+# output "kube_slave_public_ip" {
+#     description = "Public IP's of EC2 instances"
+#     value = aws_instance.kube_slave[*].public_ip
+# }
 
-output "reactjs_public_ip" {
-    description = "Public IPs of reactjs instance"
-    value = aws_instance.reactjs_server.public_ip
-}
+# output "reactjs_public_ip" {
+#     description = "Public IPs of reactjs instance"
+#     value = aws_instance.reactjs_server.public_ip
+# }
 
-output "nodejs_public_ip" {
-    description = "Public IPs of nodejs instance"
-    value = aws_instance.nodejs_server.public_ip
-}
+# output "nodejs_public_ip" {
+#     description = "Public IPs of nodejs instance"
+#     value = aws_instance.nodejs_server.public_ip
+# }
