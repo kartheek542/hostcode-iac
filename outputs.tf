42a43e59c7ca0f602b3ca463354df002b9982de7
kartheek542 <kartheekdivvela@gmail.com>
2025-01-19 00:45:35 +0530

    uncommented kube slaves

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,11 @@ output "kube_master_public_ip" {
     value = aws_instance.kube_master.public_ip
 }
 
-# output "kube_slave_public_ip" {
-#     description = "Public IP's of EC2 instances"
-#     value = aws_instance.kube_slave[*].public_ip
-# }
-# 
+output "kube_slave_public_ip" {
+    description = "Public IP's of EC2 instances"
+    value = aws_instance.kube_slave[*].public_ip
+}
+
 # output "reactjs_public_ip" {
 #     description = "Public IPs of reactjs instance"
 #     value = aws_instance.reactjs_server.public_ip
