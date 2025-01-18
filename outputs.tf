0c417317a63d6e8735388b43e8a3009181317b5a
kartheek542 <kartheekdivvela@gmail.com>
2025-01-18 11:21:54 +0530

    test

--- outputs.tf
+++ outputs.tf
@@ -7,12 +7,12 @@ output "kube_master_public_ip" {
 #     description = "Public IP's of EC2 instances"
 #     value = aws_instance.kube_slave[*].public_ip
 # }
-
+# 
 # output "reactjs_public_ip" {
 #     description = "Public IPs of reactjs instance"
 #     value = aws_instance.reactjs_server.public_ip
 # }
-
+# 
 # output "nodejs_public_ip" {
 #     description = "Public IPs of nodejs instance"
 #     value = aws_instance.nodejs_server.public_ip
